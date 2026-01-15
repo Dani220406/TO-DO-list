6d6f75aef2b3c1ea9429975f6bb475a003f225a5
Daniele Conti <conti.daniele.220406@gmail.com>
2026-01-15 11:46:21 +0100

    fix: mistakenly left a setting outside main

--- todo_app/main.py
+++ todo_app/main.py
@@ -3,10 +3,9 @@ from todo_app.config import init_session_state
 from todo_app.screens.welcome import welcome_screen
 from todo_app.screens.home import home
 
-st.set_page_config(layout="wide")
-
 
 def main():
+    st.set_page_config(layout="wide")
     init_session_state()
     if st.session_state.vista == "welcome":
         welcome_screen()
